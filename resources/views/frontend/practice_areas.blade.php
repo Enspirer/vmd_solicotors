aac2801a551ec3f6fc9b992a7c7189b7b2ac12ed
ACHii <achintha.tallentor@gmail.com>
2022-06-06 18:16:27 +0530

    hero banner

--- resources/views/frontend/practice_areas.blade.php
+++ resources/views/frontend/practice_areas.blade.php
@@ -10,6 +10,7 @@
             <div class="title-block">
                 <div class="subtitle">Our Specialties</div>
                 <div class="title">Areas of <br> Practice</div>
+                <a href="#" class="cta-btn btn-fill">Contact us</a>
             </div>
             <div class="text-block">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also</div>
         </div>
